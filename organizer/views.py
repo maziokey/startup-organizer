b7670d91a3766abf1fa70bed07c01cd6ab93c966
maziokey <okeyluvsu2004@yahoo.com>
2017-09-25 12:26:41 +0100

    my startup organizer blog, first commit

---
+++ organizer/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404, render
+#from django.http.response import HttpResponse, Http404
+#from django.template import Context, loader
+
+from .models import Tag, Startup
+
+# Create your views here.
+def tag_list(request):
+    return render(request, 'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(request, 'organizer/tag_detail.html', {'tag': tag})
+
+def startup_list(request):
+    return render(request, 'organizer/startup_list.html', {'startup_list': Startup.objects.all()})
+
+def startup_detail(request, slug):
+    startup = get_object_or_404(Startup, slug__iexact=slug)
+    return render(request, 'organizer/startup_detail.html', {'startup': startup})
