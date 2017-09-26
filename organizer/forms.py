2afb9d6a458c6f9ccd9a961967675a06cdeb01a2
maziokey <okeyluvsu2004@yahoo.com>
2017-09-26 14:38:15 +0100

    created forms.py for blog app.

--- organizer/forms.py
+++ organizer/forms.py
@@ -1,9 +1,31 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
-class TagForm(forms.Form):
-    name = forms.CharField(max_length=31)
-    slug = forms.SlugField(max_length=31, help_text='A label for URL config')
+from .models import Tag, NewsLink, Startup
 
-    def save(self):
-        new_tag = Tag.objects.create(name=self.cleaned_data['name'], slug=self.cleaned_data['slug'])
-        return new_tag
+class TagForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    def clean_name(self):
+        return self.cleaned_data['name'].lower()
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+class SlugCleanMixin:
+    """Mixin class for slug cleaning method."""
+
+    def clean_slug(self):
+        new_slug = (self.cleaned_data['slug'].lower())
+        if new_slug == 'create':
+            raise ValidationError('Slug may not be "create".')
+        return new_slug
