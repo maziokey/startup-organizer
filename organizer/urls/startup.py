8759cdd8788095c4457e2c4ab0230055b46cc05b
maziokey <okeyluvsu2004@yahoo.com>
2017-11-22 12:05:26 +0100

    Implemented pagination for startup_list page.

--- organizer/urls/startup.py
+++ organizer/urls/startup.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from ..views import StartupCreate, StartupDelete, StartupUpdate, startup_detail, startup_list
+from ..views import StartupCreate, StartupDelete, StartupUpdate, StartupList, startup_detail
 
 urlpatterns = [
-    url(r'^$', startup_list, name='organizer_startup_list'),
+    url(r'^$', StartupList.as_view(), name='organizer_startup_list'),
     url(r'^create/$', StartupCreate.as_view(), name='organizer_startup_create'),
     url(r'^(?P<slug>[\w\-]+)/$', startup_detail, name='organizer_startup_detail'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', StartupDelete.as_view(), name='organizer_startup_delete'),
--- organizer/views.py
+++ organizer/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
 from .utils import ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 #from django.http.response import HttpResponse, Http404
 #from django.template import Context, loader
 
@@ -16,8 +17,49 @@ def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
     return render(request, 'organizer/tag_detail.html', {'tag': tag})
 
-def startup_list(request):
-    return render(request, 'organizer/startup_list.html', {'startup_list': Startup.objects.all()})
+#def startup_list(request):
+#    return render(request, 'organizer/startup_list.html', {'startup_list': Startup.objects.all()})
+
+class StartupList(View):
+    page_kwarg = 'page'
+    paginate_by = 5 # 5 items per page
+    template_name = 'organizer/startup_list.html'
+
+    def get(self, request):
+        startups = Startup.objects.all()
+        paginator = Paginator(
+            startups, self.paginate_by)
+        page_number = request.GET.get(
+            self.page_kwarg)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.previous_page_number())
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = "?{pkw}={n}".format(
+                pkw=self.page_kwarg,
+                n=page.next_page_number())
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'startup_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 def startup_detail(request, slug):
     startup = get_object_or_404(Startup, slug__iexact=slug)
