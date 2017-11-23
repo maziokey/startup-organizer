0cc0567345d61817320ee65a35a0645708f6a1a1
maziokey <okeyluvsu2004@yahoo.com>
2017-11-23 11:37:06 +0100

    created pagination for tag_list.

--- organizer/urls/tag.py
+++ organizer/urls/tag.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from ..views import tag_list, tag_detail, TagCreate, TagDelete, TagUpdate
+from ..views import TagCreate, TagDelete, TagList, TagPageList, TagUpdate, tag_detail
 
 urlpatterns = [
-    url(r'^$', tag_list, name='organizer_tag_list'),
+    url(r'^(?P<page_number>\d+)/$', TagPageList.as_view(), name='organizer_tag_page'),
+    url(r'^$', TagList.as_view(), name='organizer_tag_list'),
     url(r'^create/$', TagCreate.as_view(), name='organizer_tag_create'),
     url(r'^(?P<slug>[\w\-]+)/$', tag_detail, name='organizer_tag_detail'),
     url(r'^(?P<slug>[\w\-]+)/delete/$', TagDelete.as_view(), name='organizer_tag_delete'),
--- organizer/views.py
+++ organizer/views.py
@@ -10,8 +10,61 @@ from .models import Tag, Startup, NewsLink
 from .forms import TagForm, StartupForm, NewsLinkForm
 
 # Create your views here.
-def tag_list(request):
-    return render(request, 'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+#def tag_list(request):
+#    return render(request, 'organizer/tag_list.html', {'tag_list': Tag.objects.all()})
+
+class TagList(View):
+    template_name = 'organizer/tag_list.html'
+
+    def get(self, request):
+        tags = Tag.objects.all()
+        context = {
+            'tag_list': tags,
+        }
+        return render(
+            request, self.template_name, context)
+
+class TagPageList(View):
+    paginate_by = 5
+    template_name = 'organizer/tag_list.html'
+
+    def get(self, request, page_number):
+        tags = Tag.objects.all()
+        paginator = Paginator(
+            tags, self.paginate_by)
+        try:
+            page = paginator.page(page_number)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            page = paginator.page(
+                paginator.num_pages)
+        if page.has_previous():
+            prev_url = reverse(
+                'organizer_tag_page',
+                args=(
+                    page.previous_page_number(),
+                ))
+        else:
+            prev_url = None
+        if page.has_next():
+            next_url = reverse(
+                'organizer_tag_page',
+                args=(
+                    page.next_page_number(),
+                ))
+        else:
+            next_url = None
+        context = {
+            'is_paginated':
+                page.has_other_pages(),
+            'next_page_url': next_url,
+            'paginator': paginator,
+            'previous_page_url': prev_url,
+            'tag_list': page,
+        }
+        return render(
+            request, self.template_name, context)
 
 def tag_detail(request, slug):
     tag = get_object_or_404(Tag, slug__iexact=slug)
